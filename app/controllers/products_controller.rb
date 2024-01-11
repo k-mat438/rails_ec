d38e7cc9239ac1595c251b7fcdfe124d0738c582
k-mat438 <a1sco00ocs1a@gmail.com>
2024-01-11 21:00:11 +0900

    add item_index

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.order(created_at: :desc).all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+
+    @products = Product.order(created_at: :desc).where.not(id: params[:id]).limit(4)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,65 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product1 = Product.new(
+  name: "Fancy Product",
+  price: "$40.00 - $80.00"
+)
+product1.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product1.save!
+
+
+product2 = Product.new(
+  name: "Special Item",
+  price: "$18.00"
+)
+product2.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product2.save!
+
+
+product3 = Product.new(
+  name: "Sale Item",
+  price: "$25.00"
+)
+product3.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product3.save!
+
+
+product4 = Product.new(
+  name: "Popular Item",
+  price: "$40.00"
+)
+product4.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product4.save!
+
+
+product5 = Product.new(
+  name: "Sale Item",
+  price: "$25.00"
+)
+product5.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product5.save!
+
+product6 = Product.new(
+  name: "Fancy Product",
+  price: "$120.00 - $280.00"
+)
+product6.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product6.save!
+
+
+product7 = Product.new(
+  name: "Special Item",
+  price: "$18.00"
+)
+product7.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product7.save!
+
+
+product8 = Product.new(
+  name: "Popular Item",
+  price: "$40.00"
+)
+product8.image.attach(io: File.open(Rails.root.join('app/assets/images/image.jpg')), filename: 'image.jpg')
+product8.save!
