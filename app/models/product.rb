b4dfaa478245389825214cd795c53f573c7cf5dd
k-mat438 <a1sco00ocs1a@gmail.com>
2024-01-22 19:23:41 +0900

    add destroy controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true, blob: { content_type: :image }
 end
