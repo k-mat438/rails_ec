77ec1cc667b1da598b0b18b9b4b955b133537850
k-mat438 <a1sco00ocs1a@gmail.com>
2024-01-28 11:59:15 +0900

    change

---
+++ spec/mailers/checkout_mailer_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CheckoutMailer, type: :mailer do
+  describe 'creation_email' do
+    let(:mail) { described_class.creation_email }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Creation email')
+      # expect(mail.to).to eq(['to@example.org'])
+      # expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hi')
+    end
+  end
+end
